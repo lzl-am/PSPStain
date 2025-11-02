145be91aa08946a211b6a5f06b5e38e2350f098a
lzl-am <zilonglinam@163.com>
2025-11-02 15:40:36 +0800

    fix: 修改代码，以便测试虚拟染色在验证集上的效果

--- options/test_options.py
+++ options/test_options.py
@@ -14,6 +14,8 @@ class TestOptions(BaseOptions):
         # Dropout and Batchnorm has different behavioir during training and test.
         parser.add_argument('--eval', action='store_true', help='use eval mode during test time.')
         parser.add_argument('--num_test', type=int, default=50, help='how many test images to run')
+        parser.add_argument('--unet_seg', type=str, default='', 
+                    help='Specify the type of UNet segmentation model, e.g., MIST_unet_seg')
 
         # To avoid cropping, the load_size should be the same as crop_size
         parser.set_defaults(load_size=parser.get_default('crop_size'))
